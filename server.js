08832f37e1a19221f4bd4b49225efa8087cdcb11
M4RKL4B0Y <markvlaboy@gmail.com>
2024-02-08 20:36:08 -0500

    languages

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
-)
+
 
 // MIDDLEWARE
 app.use(express.urlencoded({extended: true}))
+
+// LANGUAGES
+const languagesController = require('./controllers/languages_controller.js')
+app.use('/languages', languagesController)
 
 // ROUTES
 app.get('/', (req, res) => {
@@ -22,3 +24,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log('Greetings! From port: ', PORT);
 })
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log('connected to mongo: ', process.env.MONGO_URI))
